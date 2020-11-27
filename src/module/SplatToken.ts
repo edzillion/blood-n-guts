2dae9346634cc2cd75a915df7e6d62c5678a6d20
edzillion <edzillion@gmail.com>
2020-11-27 15:31:08 +0100

    cleanup

--- src/module/SplatToken.ts
+++ src/module/SplatToken.ts
@@ -191,7 +191,6 @@ export default class SplatToken {
         this.tokenSettings[setting] = changes.flags[MODULE_ID][setting];
       }
     }
-    debugger;
 
     this.disabled = this.tokenSettings.bloodColor === 'none' || this.tokenSettings.violenceLevel === 'Disabled';
 
